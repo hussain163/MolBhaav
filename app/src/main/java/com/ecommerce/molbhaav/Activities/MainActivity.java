c7c42ff4b3459669737e7066f4fe0b45a66a7d28
Hussain Poonawala <hussain.poonawala163@gmail.com>
2019-01-20 16:17:51 +0530

    Initial Commit

---
+++ app/src/main/java/com/ecommerce/molbhaav/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ecommerce.molbhaav.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ecommerce.molbhaav.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/ecommerce/molbhaav/Controller/AppControl.java
@@ -0,0 +1,25 @@
+package com.ecommerce.molbhaav.Controller;
+
+import android.app.Application;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AppControl extends Application {
+
+    public static Retrofit retrofit = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (retrofit == null){
+            OkHttpClient client = new OkHttpClient.Builder().build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://10.0.2.2:8000")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client).build();
+        }
+    }
+}
